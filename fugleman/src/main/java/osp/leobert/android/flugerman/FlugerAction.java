a32d2c595fb5fdc62b9f6b573561dc99b9c48a36
leobert-lan <774057695@qq.com>
2019-01-15 10:45:23 +0800

    rename

---
+++ fugleman/src/main/java/osp/leobert/android/flugerman/FlugerAction.java
@@ -0,0 +1,11 @@
+package osp.leobert.android.flugerman;
+
+/**
+ * <p><b>Package:</b> osp.leobert.android.flugerman </p>
+ * <p><b>Project:</b> Flugerman </p>
+ * <p><b>Classname:</b> FlugerAction </p>
+ * <p><b>Description:</b> TODO </p>
+ * Created by leobert on 2019/1/15.
+ */
+public class FlugerAction {
+}
